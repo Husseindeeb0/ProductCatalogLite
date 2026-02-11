a11dc8e11df8caebb8e7ce5e7ca01447bef16d65
Hussein <deebh776@gmail.com>
2026-02-11 17:23:11 +0200

    Add fetch api function & setup pinia & display products into initial design & add search filtering

--- src/stores/productStore.ts
+++ src/stores/productStore.ts
@@ -4,4 +4,24 @@ import type { ProductType } from "@/types/productType";
 
 export const useProductStore = defineStore('productStore', () => {
   const products = ref<ProductType[]>([]);
+  const loading = ref<boolean>(false);
+
+  const fetchProducts = async () => {
+    try {
+      loading.value = true;
+      const response = await fetch('https://fakestoreapi.com/products');
+      const data = await response.json();
+      products.value = data;
+    } catch (error) {
+      console.log("Error fetching products: ", error);
+    } finally {
+      loading.value = false;
+    }
+  }
+
+  return {
+    products,
+    loading,
+    fetchProducts
+  }
 })
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,67 @@
 <script setup lang="ts">
+import { useProductStore } from '@/stores/productStore'
+import { onMounted } from 'vue'
+import { storeToRefs } from 'pinia'
+import { ref } from 'vue'
+import { computed } from 'vue'
+
+const store = useProductStore()
+const { products, loading } = storeToRefs(store)
+const { fetchProducts } = store
+
+onMounted(() => {
+  console.log('running')
+  fetchProducts()
+})
+
+// Search state used to store user final search input value
+const search = ref<string>('')
+// Search state used to store user ongoing search input value
+// So that filtering is not going to be triggered on every keystroke because of using computed
+const activeSearch = ref<string>('')
+
+const handleSearch = () => {
+  activeSearch.value = search.value
+}
+
+// computed is going to trigger refiltering whenever activeSearch or products values changes
+const filteredProducts = computed(() => {
+  return products.value.filter((product) =>
+    product.title.toLowerCase().includes(activeSearch.value.toLowerCase()),
+  )
+})
 </script>
 
 <template>
   <main>
+    <div class="flex gap-5 justify-center my-10">
+      <form @submit.prevent="handleSearch">
+        <input type="text" v-model="search" placeholder="Search" />
+        <button type="submit">Search</button>
+      </form>
+    </div>
+    <div class="flex flex-wrap gap-5 justify-center my-10">
+      <div
+        v-for="product in filteredProducts"
+        :key="product.id"
+        class="w-64 h-80 border-2 border-gray-200 rounded-2xl p-5 overflow-auto"
+      >
+        <img :src="product.image" alt="Preview" class="max-h-32 mx-auto" />
+        <h1 class="text-center font-bold">
+          {{ product.title }}
+        </h1>
+        <span class="mx-auto"> {{ product.price }}$ </span>
+        <p class="text-sm line-clamp-3">
+          {{ product.description }}
+        </p>
+        <span>
+          {{ product.category }}
+        </span>
+        <div class="flex gap-5">
+          <span> Count: {{ product.rating.count }} </span>
+          <span> Rate: {{ product.rating.rate }} </span>
+        </div>
+      </div>
+    </div>
   </main>
 </template>
